fa6bab5e542853317d7c9e4fe95ace460c4d4724
Adam Booth <asbooth@outlook.com>
2018-07-04 17:42:45 +0100

    Revert "componentDidReceiveProps isn't a lifecycle method lol"
    This reverts commit 0d0189d499529f0750a378e4ba87da67f6097e69.

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -87,7 +87,7 @@ export default Component => {
     }
 
     // We have to make sure the validate method is kept when new props are added
-    componentWillReceiveProps(nextProps) {
+    componentDidReceiveProps(nextProps) {
       this.setValidations(nextProps.validations, nextProps.required);
     }
 
